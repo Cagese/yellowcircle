3f80edfaf6e49c0a8f9648ec207b81f826651d7e
Cagese <mikhail_morozov_2006@mail.ru>
2023-11-21 18:12:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+from math import sin, cos, pi
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Suprematism(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.setMouseTracking(True)
+        self.qp = QPainter()
+        self.flag = False
+        self.status = None
+        self.yellowmagic.clicked.connect(self.drawf)
+
+    def drawf(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw(self.status)
+            self.qp.end()
+
+    def draw(self, status):
+        self.coords_ = [400,300]
+        R = randint(20, 300)
+        self.qp.setBrush(QColor(255,255,0))
+        self.qp.drawEllipse(int(self.coords_[0] - R / 2),
+                            int(self.coords_[1] - R / 2), R, R)
+
+
+
+    def initUI(self):
+        self.setGeometry(300, 300, 1000, 1000)
+        self.setWindowTitle('Рисование')
+
+    def mousePressEvent(self, event):
+        self.coords_ = [event.x(), event.y()]
+        if (event.button() == Qt.LeftButton):
+            self.status = 1
+        elif (event.button() == Qt.RightButton):
+            self.status = 2
+        self.drawf()
+
+    def mouseMoveEvent(self, event):
+        self.coords_ = [event.x(), event.y()]
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.status = 3
+            self.drawf()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec_())
